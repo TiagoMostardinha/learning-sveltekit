ff98ed4c352708afcf619f5850d9664149e71c24
Tiago Mostardinha <tiago.mostardinha@ua.pt>
2023-08-14 21:34:36 +0100

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+</script>
+
+<div class="index">
+    <h2>Some Title</h2>
+    <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores
+        libero aperiam ea eos! Tempore molestiae sapiente adipisci, error
+        voluptatibus impedit ipsam eos quia, beatae et commodi, modi eligendi
+        odio porro! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Officiis debitis numquam atque cum aliquam dignissimos nam tempora
+        facilis et. Fuga dolore placeat quos delectus laborum ipsam dicta? Eum,
+        ratione tempora.
+    </p>
+    <a href="/guides">View Guides</a>
+    <a href="/about">About</a>
+</div>
+
+<style>
+    .index {
+        text-align: center;
+        display: block;
+        margin: 20px auto;
+    }
+</style>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+</script>
+
+<div class="about">
+    <h2>About Us</h2>
+    <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus
+        exercitationem expedita quia illo sequi molestiae quos voluptate
+        sapiente a at ea, vitae dicta fugiat! Eum, iste magnam! Porro, deserunt
+        quisquam! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Reiciendis cupiditate quibusdam nostrum maiores voluptas exercitationem
+        facere veniam animi porro tempore! Quod quae sapiente deleniti quasi
+        recusandae aperiam cumque possimus deserunt!
+    </p>
+    <a href="/">Home</a>
+    <a href="/guides">View Guides</a>
+</div>
+
+<style>
+    .about {
+        text-align: center;
+        display: block;
+        margin: 20px auto;
+    }
+</style>
